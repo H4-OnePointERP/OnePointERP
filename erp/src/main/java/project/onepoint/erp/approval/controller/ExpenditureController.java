12526e3a83e382852daf202236339a13720fb1ec
hamings <wjdgpal9630@gmail.com>
2023-09-01 12:25:16 +0900

    #18 feat: 지출결의서 등록 API 구현

--- erp/src/main/java/project/onepoint/erp/approval/controller/ExpenditureController.java
+++ erp/src/main/java/project/onepoint/erp/approval/controller/ExpenditureController.java
@@ -1,7 +1,40 @@
 package project.onepoint.erp.approval.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import project.onepoint.erp.approval.dto.req.ExpenditureReq;
+import project.onepoint.erp.approval.dto.res.ExpenditureRes;
+import project.onepoint.erp.approval.service.ExpenditureService;
 
+@RequiredArgsConstructor
 @Controller
 public class ExpenditureController {
+
+    private final ExpenditureService expenditureService;
+
+
+    /**
+     * 지출결의서 등록하는 API
+     * @param expenditureReq: 지출결의서 등록 DTO
+     * @param model: 지출결의서 상세보기 정보
+     * @return: view
+     */
+    @PostMapping("/approval/expenditure")
+    public String insertExpenditure(@ModelAttribute ExpenditureReq expenditureReq, Model model){
+        ExpenditureRes res = expenditureService.insertExpenditure(expenditureReq);
+        if(res!=null){
+            model.addAttribute("expenditureRes",res);
+            return "expenditureDetail";
+        }else {
+            return "expenditureForm";
+        }
+
+    }
+
+
+
+
 }
