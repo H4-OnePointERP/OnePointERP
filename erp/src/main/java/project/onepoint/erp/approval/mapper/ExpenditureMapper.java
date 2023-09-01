9c34592d03da106d0f03898f7c7ec1614a829f34
hamings <wjdgpal9630@gmail.com>
2023-09-01 12:24:13 +0900

    #18 feat: 지출결의서 등록 비즈니스로직 구현

--- erp/src/main/java/project/onepoint/erp/approval/mapper/ExpenditureMapper.java
+++ erp/src/main/java/project/onepoint/erp/approval/mapper/ExpenditureMapper.java
@@ -1,7 +1,13 @@
 package project.onepoint.erp.approval.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import project.onepoint.erp.approval.dto.req.ExpenditureReq;
+import project.onepoint.erp.approval.dto.res.ExpenditureRes;
 
 @Mapper
 public interface ExpenditureMapper {
+
+    int insertExpenditure(ExpenditureReq req);
+
+    ExpenditureRes selectExpenditureByAppSeq(int appSeq);
 }
