a13a426840aeaa341e0ab18a9295d9d7367c78b7
unknown <dongjooyoung@naver.com>
2023-09-04 10:23:48 +0900

    [Feat]  품으ã의서 ë¹서 비지 비즈니스 로직 구현, LeaveMapper/LoaMapper 쿼ë쿼리문 작성

--- erp/src/main/java/project/onepoint/erp/approval/mapper/LoaMapper.java
+++ erp/src/main/java/project/onepoint/erp/approval/mapper/LoaMapper.java
@@ -1,7 +1,13 @@
 package project.onepoint.erp.approval.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import project.onepoint.erp.approval.dto.req.LoaReq;
+import project.onepoint.erp.approval.dto.res.LoaRes;
 
 @Mapper
 public interface LoaMapper {
+
+    int insertLoa(LoaReq req);
+
+    LoaRes selectLoaByAppSeq(int appSeq);
 }
